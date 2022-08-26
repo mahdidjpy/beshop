fe0331e03d7b1c2ba4389f2269640fb0c2719bc1
mahdi <mahpython@gmail.com>
2022-08-26 22:28:32 +0430

    create model and some config

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Product, Gender, Condition
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Gender)
+admin.site.register(Condition)
+
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+
+class Gender(models.Model):
+	GENDER_CHOICE = (
+			('ma' , 'مرد'),
+			('fe' , 'زن'),
+		)
+
+	gender = models.CharField(max_length=2, choices=GENDER_CHOICE, verbose_name='جنسیت' )
+
+
+	def __str__(self):
+		return self.gender
+
+
+class Category(models.Model):
+	title = models.CharField(max_length=30, verbose_name='عنوان')
+	status = models.BooleanField(verbose_name='وضعیت انتشار', default=True)
+
+
+	def __str__(self):
+		return self.title
+
+
+class Condition(models.Model):
+	title = models.CharField(max_length=30, verbose_name='عنوان')
+	status = models.BooleanField(verbose_name='وضعیت انتشار', default=True)
+
+
+	def __str__(self):
+		return self.title
+
+
+
+class Product(models.Model):
+	STATUS_CHOICE = (
+			('p',"انتشار"),
+        ('d',"پیش نویس"),
+        ('l',"درحال برسی"),
+        ('b',"برگشت داده شده"),
+		)
+
+
+	title = models.CharField(max_length=50, verbose_name='عنوان')
+	slug = models.SlugField(max_length=20, null=True, unique=True, allow_unicode=True)
+	gender = models.ManyToManyField(Gender ,verbose_name='جنسیت', )
+	description = models.TextField(verbose_name='توضیحات')
+	price = models.PositiveIntegerField(verbose_name='قیمت')
+	image = models.ImageField(default=None, upload_to='images')
+	created = models.DateTimeField(auto_now_add=True, verbose_name='زمان اجاد')
+	status = models.CharField(max_length=1, choices=STATUS_CHOICE, verbose_name='وضعیت انتشار')
+	condition = models.ManyToManyField(Condition, verbose_name='حالت')
+	owner = models.ForeignKey(User, verbose_name='مالک', null=True, on_delete=models.SET_NULL)
+	category = models.ManyToManyField(Category ,verbose_name='دسته بندی' )	
+
+
+	def __str__(self):
+		return self.title
+
